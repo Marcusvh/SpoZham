a44acc55911ac68a0656dd43012301666b9e87a1
Patrick Ellesgaard Ring <patr4174@edu.easj.dk>
2022-12-01 14:21:05 +0100

    Ikke mine ændringer

---
+++ SpoZhamVue/js/SongInfo.js
@@ -0,0 +1,23 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '0f6eff705dmshf162a9814c66ba5p13b8c4jsnef8afa48c048',
+        'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+    }
+};
+
+var trackId
+
+fetch("http://localhost:5204/api/")
+
+
+
+fetch('https://shazam-core.p.rapidapi.com/v1/tracks/details?track_id=491697169', options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        document.getElementById('song').innerHTML = response.title
+        document.getElementById('artist').innerHTML = response.subtitle
+        document.getElementById('release').innerHTML = response.releasedate
+    })
+    .catch(err => console.error(err));
