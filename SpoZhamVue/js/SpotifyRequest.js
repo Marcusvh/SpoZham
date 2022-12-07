81d9e34eda210914724c6aceb586faf00654c269
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-12-07 14:07:01 +0100

    welp

--- SpoZhamVue/js/SpotifyRequest.js
+++ SpoZhamVue/js/SpotifyRequest.js
@@ -4,31 +4,56 @@ async function token() {
     return axios.get(`http://localhost:5204/api/User/Spotify/Token?id=${testId}`)
 }
 
+// let nameing = await token() .then(response => {return response.data})
+// nameing.access // for token
+// nameing.id // for spotify user id
+// nameing.timeStamp // timestamp for hvornår token kom i databasen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * laver et api kald til spotify hvor vi lægger en sang i en playliste
  */
-async function AddSongToPlaylist() {
-    let playlistID = "42xr6CZ4SlVULt5BGaFPrK"
+// async function AddSongToPlaylist() {
+//     let playlistID = "42xr6CZ4SlVULt5BGaFPrK"
 
-    let Atoken = await token()
-    .then(response => {return response.data})
+//     let Atoken = await token()
+//     .then(response => {return response.data})
 
-    console.log(Atoken);
-    await fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${Atoken.access}`
-        },
-        body: JSON.stringify({
-            "uris": ["spotify:track:7iC9PkiqWeN7E3WWh2dU5P", "spotify:track:6rmXhRIemCTPyMYZRDN7Qg"]
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
+//     console.log(Atoken);
+//     await fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Authorization": `Bearer ${Atoken.access}`
+//         },
+//         body: JSON.stringify({
+//             "uris": ["spotify:track:7iC9PkiqWeN7E3WWh2dU5P", "spotify:track:6rmXhRIemCTPyMYZRDN7Qg"]
+//         })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
     
-    })
+//     })
     
-}
+// }
 // AddSongToPlaylist()
