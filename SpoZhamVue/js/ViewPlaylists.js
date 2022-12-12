36ac8958d929a55400423fb0f6efeb3984ade6a1
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-12-12 14:24:19 +0100

    Fejlhåndtering på ViewPlaylists

--- SpoZhamVue/js/ViewPlaylists.js
+++ SpoZhamVue/js/ViewPlaylists.js
@@ -5,7 +5,7 @@ fetch('https://api.spotify.com/v1/users/1110330469/playlists?limit=50',
 {
     method : 'GET',
     headers: {
-        'Authorization': 'Bearer BQAZgbSUIwlw2k4jSt2AyIu2ZFr7U0DJdNG9EkZxaiRy9fnXZS9kgw4mrknhbiCTbV9H4T1Rmn_tX_yYvktsjgzsq0nM39HoxMYOxb8793RN7UV7rkb82JRERp3VEUuPtEwldTZjtQFsgvTPt2gReKVHdgyFPJxNgX96XocA2eJgtZxHQNz5s7XwrNBb', 
+        'Authorization': 'Bearer BQDuZhEdyv_xlFYRP0g2QbhR5WRdfXXwpL3ZI7nzOKpJ2ifzdksWBdm9Zsev_dYVx3wLYE3rhNIoRjerVcCbaggeB9z9kK-2tmkDFi8Ro6BU0_MS7-Sc7Qn3TdIk-yJeRzDPXVZdu603dDDda6eGM84y6ytYlUe7L1enjZiyPb5rXwC3f5HYXjPICifvs2Bc2-Q', 
         'Content-Type': 'application/json',
     },
 })
@@ -13,13 +13,10 @@ fetch('https://api.spotify.com/v1/users/1110330469/playlists?limit=50',
 /**
  * Printer responset ud som en Json.
  */
-.then(response => response.json())
-
-/**
- * Får vist playlisterne som en liste (Vises i venstre side)
- */
+.then(response => response.json()) 
 .then(response => {
-
+    try 
+    {
     /**
      * Printer responset ud i consolen.
      */
@@ -33,7 +30,16 @@ fetch('https://api.spotify.com/v1/users/1110330469/playlists?limit=50',
             console.log(element)
             makeOption(element.name, 'value', element.id)
             makeP(element.name, '', '')
-        });    
+        })
+    }
+    /**
+     * Hvis playlisterne ikke kommer frem eller findes kastes dette.
+     */
+    catch(error)
+    {
+        document.getElementById("error").innerHTML = 'Noget gik galt, bruger har ingen playlister eller forkert bruger'
+        document.getElementById("error").removeAttribute("hidden")
+    }    
 })
 
 /**
@@ -44,34 +50,59 @@ function makeOption(text, attr, attrValue)
     /**
      * Henter ID fra AllPlaylists.
      */
-    let parentAppend = document.getElementById('AllPlaylists')
+        let parentAppend = document.getElementById('AllPlaylists')
 
     /**
      * Laver Drop down options.
      */
-    let pText;
-    const p = document.createElement('option')
+        let pText;
+        const p = document.createElement('option')
 
     /**
      * Tjekker om teksten ikke er tom eller null, før vi begynder og lave en ny.
      */
-    if(text != "" && text != null)
-    {
-        pText = document.createTextNode(text)
-        p.append(pText)
-    }
+        if(text != "" && text != null)
+        {
+            pText = document.createTextNode(text)
+            p.append(pText)
+        }
 
     /**
      * Tjekker om attributen ikke er tom eller null ellers sætter den attributen.
      */
-    if(attr != "" && attr != null) 
-    {
-        p.setAttribute(attr, attrValue)
-    }
+        if(attr != "" && attr != null) 
+        {
+            p.setAttribute(attr, attrValue)
+        }
 
     /**
      * Tilføjer attributen p til Alle playlister (Parent).
      */
+        parentAppend.appendChild(p)
+    
+    
+}
+
+/**
+ * Funktion til visning af playlister i venstre side.
+ * @param {text} text 
+ * @param {attribute} attr 
+ * @param {attributevalue} attrValue 
+ */
+function makeP(text, attr, attrValue) {
+    let parentAppend = document.getElementById('ShowPlaylists')
+
+    let pText;
+    const p = document.createElement('h4')
+    if(text != "" && text != null){
+        pText = document.createTextNode(text)
+        p.append(pText)
+    }
+
+    if(attr != "" && attr != null) {
+        p.setAttribute(attr, attrValue)
+    }
+
     parentAppend.appendChild(p)
 }
 
