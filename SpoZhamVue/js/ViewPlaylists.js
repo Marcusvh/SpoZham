0cda7d74bc03554bd9828f56e2f251d77edc2108
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-12-12 10:48:14 +0100

    Dokumentation for ViewPlaylist.js done

--- SpoZhamVue/js/ViewPlaylists.js
+++ SpoZhamVue/js/ViewPlaylists.js
@@ -1,17 +1,34 @@
 /**
- * henter en brugers 50 nyeste playlister ud fra deres bruger id
+ * Henter en brugers 50 nyeste playlister ud fra deres bruger id
  */
-fetch('https://api.spotify.com/v1/users/1110330469/playlists?limit=50',{
+fetch('https://api.spotify.com/v1/users/1110330469/playlists?limit=50',
+{
     method : 'GET',
     headers: {
         'Authorization': 'Bearer BQAZgbSUIwlw2k4jSt2AyIu2ZFr7U0DJdNG9EkZxaiRy9fnXZS9kgw4mrknhbiCTbV9H4T1Rmn_tX_yYvktsjgzsq0nM39HoxMYOxb8793RN7UV7rkb82JRERp3VEUuPtEwldTZjtQFsgvTPt2gReKVHdgyFPJxNgX96XocA2eJgtZxHQNz5s7XwrNBb', 
         'Content-Type': 'application/json',
     },
 })
+
+/**
+ * Printer responset ud som en Json.
+ */
 .then(response => response.json())
+
+/**
+ * Får vist playlisterne som en liste (Vises i venstre side)
+ */
 .then(response => {
+
+    /**
+     * Printer responset ud i consolen.
+     */
         console.log(response)
-        const playlists = response.items    
+        const playlists = response.items
+
+        /**
+         * Går igennem listen af playlister, printer dem ud i consolen og laver options i drop-down menuen.  
+         */   
         playlists.forEach(element => { 
             console.log(element)
             makeOption(element.name, 'value', element.id)
@@ -20,46 +37,43 @@ fetch('https://api.spotify.com/v1/users/1110330469/playlists?limit=50',{
 })
 
 /**
- * printer en brugers playlister ud under sang informationen
+ * Funktion til at printe en brugers playlister ud under sang informationen (drop down)
  */
-function makeOption(text, attr, attrValue) {
+function makeOption(text, attr, attrValue) 
+{
+    /**
+     * Henter ID fra AllPlaylists.
+     */
     let parentAppend = document.getElementById('AllPlaylists')
 
+    /**
+     * Laver Drop down options.
+     */
     let pText;
     const p = document.createElement('option')
-    if(text != "" && text != null){
+
+    /**
+     * Tjekker om teksten ikke er tom eller null, før vi begynder og lave en ny.
+     */
+    if(text != "" && text != null)
+    {
         pText = document.createTextNode(text)
         p.append(pText)
     }
 
-    if(attr != "" && attr != null) {
+    /**
+     * Tjekker om attributen ikke er tom eller null ellers sætter den attributen.
+     */
+    if(attr != "" && attr != null) 
+    {
         p.setAttribute(attr, attrValue)
     }
 
+    /**
+     * Tilføjer attributen p til Alle playlister (Parent).
+     */
     parentAppend.appendChild(p)
 }
 
-function ViewPlaylist(){
-    let playlistID = document.getElementById("AllPlaylists").value
-    let playlistShow = document.getElementById("ShowPlaylist")
-    playlistShow.setAttribute('href', "https://open.spotify.com/playlist/" + playlistID)
-   
-}
-        
-    function makeP(text, attr, attrValue) {
-       let parentAppend = document.getElementById('ShowPlaylists')
 
-       let pText;
-       const p = document.createElement('h4')
-       if(text != "" && text != null){
-           pText = document.createTextNode(text)
-           p.append(pText)
-       }
-
-       if(attr != "" && attr != null) {
-           p.setAttribute(attr, attrValue)
-       }
-
-       parentAppend.appendChild(p)
-   }
 
